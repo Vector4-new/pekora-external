aeeb31ec7013eba6682ad9fd2f5f8105c4eec0fb
Vector4-new <68828797+Vector4-new@users.noreply.github.com>
2024-12-30 21:50:07 +0000

    feat: Detect compile errors

--- bytecode/compiler.py
+++ bytecode/compiler.py
@@ -8,6 +8,11 @@ def CompileSource(source : str):
     with open(inFilePath, "w") as f:
         f.write(source)
 
-    subprocess.run([ "./luac5.1.exe", "-o", outFilePath, inFilePath ])
+    result = subprocess.run([ "./luac5.1.exe", "-o", outFilePath, inFilePath ], capture_output=True, text=True)
+
+    if result.returncode == 1:
+        colon = result.stderr.find(":")
+
+        raise ValueError(result.stderr[colon + 1:-1])
 
     return open(outFilePath, "rb").read()
